8e4bf06be73445cbe8b05f3ee39f24e53ddc9ea7
SUMAN MANDAL <128020279+suman-max10@users.noreply.github.com>
2026-04-22 23:30:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const list = document.querySelectorAll(".list");
 function activelink() {
   list.forEach((item) => item.classList.remove("active"));
   this.classList.add("active");
-}
+
+// for active button
 list.forEach((item) => item.addEventListener("click", activelink));
